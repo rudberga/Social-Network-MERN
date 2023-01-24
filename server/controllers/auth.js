3094d51692767924bb32bce5b9bc77b50de67758
Alex <56194510+rudberga@users.noreply.github.com>
2023-01-24 16:23:59 +0900

    ADD - newUser to auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -19,6 +19,21 @@ export const register = async (req, res) => {
 		const salt = await bcrypt.genSalt();
 		const passwordHash = await bcrypt.hash(password, salt);
 
-		const newUser = new User({});
-	} catch (err) {}
+		const newUser = new User({
+			firstName,
+			lastName,
+			email,
+			password: passwordHash,
+			picturePath,
+			friends,
+			location,
+			occupation,
+			viewedProfile: Math.floor(Math.random() * 10000),
+			impressions: Math.floor(Math.random() * 10000),
+		});
+		const savedUser = await newUser.save();
+		res.status(201).json(savedUser);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 };
